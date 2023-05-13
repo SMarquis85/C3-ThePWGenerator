647871dcc5e2542facccea868767e57b7772fa98
SMarquis85 <sophienathalie_85@outlook.com>
2023-05-13 13:35:29 -0400

    updated html

--- assets/script.js
+++ assets/script.js
@@ -45,6 +45,7 @@ generateBtn.addEventListener("click", writePassword);
 
 // Define the submit event handler function
 function handleSubmit(event) {
+  
   // Prevent the default form submission behavior
   event.preventDefault();
 
