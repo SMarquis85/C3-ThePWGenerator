9a6efe856af7a0c3966dca1ba0a5481783b52af4
SMarquis85 <sophienathalie_85@outlook.com>
2023-05-17 12:49:19 -0400

    updated js

--- assets/script.js
+++ assets/script.js
@@ -14,8 +14,14 @@ function generatePassword() {
   var numberChars = "0123456789";
   var symbolChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
-  // Prompt the user for letter selection
-  var includeLetters = confirm("Do you want to include both lowercase and uppercase letters in your password?");
+  // Prompt the user for lowercase letter selection
+  var includeLowercase = confirm("Do you want to include lowercase letters in your password?");
+
+  // Prompt the user for uppercase letter selection
+  var includeUppercase = confirm("Do you want to include uppercase letters in your password?");
+
+  // Prompt the user for number selection
+  var includeNumbers = confirm("Do you want to include numbers in your password?");
 
   // Prompt the user for password length
   var passwordLength = parseInt(prompt("Enter the desired length of your password (between 8 and 128 characters):"));
@@ -31,12 +37,23 @@ function generatePassword() {
 
   // Build the character set based on user preferences
   var charset = "";
-  if (includeLetters) {
-    charset += lowercaseChars + uppercaseChars;
+  if (includeLowercase) {
+    charset += lowercaseChars;
   }
-  charset += numberChars;
+  if (includeUppercase) {
+    charset += uppercaseChars;
+  }
+  if (includeNumbers) {
+    charset += numberChars;
+  }
   if (includeSymbols) {
     charset += symbolChars;
+  }
+
+  // Check if no character types were selected
+  if (charset === "") {
+    alert("You must select at least one character type for the password.");
+    return "";
   }
 
   // Initialize an empty string to store the generated password
@@ -44,7 +61,6 @@ function generatePassword() {
 
   // Loop through the password length and randomly select characters from the charset
   for (var i = 0; i < passwordLength; i++) {
-    console.log (i)
     var randomIndex = Math.floor(Math.random() * charset.length);
     password += charset[randomIndex];
   }
@@ -61,5 +77,5 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
